709df8615f8207c65645d1a4d24b1bc3f828d367
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-03-10 21:20:38 -0600

    Login User

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,14 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(form:NgForm){
+
+    const email=form.value.email
+
+    const password=form.value.password
+
   }
 
 }
